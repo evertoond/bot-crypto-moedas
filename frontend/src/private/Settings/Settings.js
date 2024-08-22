e4e7f38511831342dcb58c34010c8bff2bd551a1
Douglas <dougboy60@hotmail.com>
2024-08-21 21:42:04 -0300

    criacao do arquivo de rotiamento(router.js) para o arquivo de login e settings

--- frontend/src/private/Settings/Settings.js
+++ frontend/src/private/Settings/Settings.js
@@ -1,6 +1,6 @@
 import React from "react";
-import { link, useHistory } from 'react-router-dom';
-function Login() {
+import { Link, useHistory } from 'react-router-dom';
+function Settings() {
     return (
         <main>
             <section className="vh-lg-100 mt-5 mt-lg-0 bg-soft d-flex align-items-center">
@@ -8,7 +8,7 @@ function Login() {
                     <p className="text-center">
                         <Link to="/" className="d-flex align-items-center justify-content-center">
                             <svg className="icon icon-xs me-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M7.707 14.707a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 1.414L5.414 9H17a1 1 0 110 2H5.414l2.293 2.293a1 1 0 010 1.414z" clipRule="evenodd"></path></svg>
-                            Back to homepage
+                            Settings
                         </Link>
                     </p>
                 </div>
@@ -16,4 +16,4 @@ function Login() {
         </main>
     )
 }
-export default Login;
+export default Settings;
